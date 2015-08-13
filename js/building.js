bccfc8bd534455fac7bd1809fe1e211b6f192e8d
Diana <97.diana@gmail.com>
2015-08-13 12:26:24 +0300

    functional rebus

--- js/building.js
+++ js/building.js
@@ -14,6 +14,22 @@ app.controller('BuildingCtrl', function ($scope, $http) {
         $scope.$apply();
     }
 
+    $scope.checkRebus = function() {
+        var ok;
+        for (var i = 0; i < $scope.rebusWords.words.length; i++) {
+            var input = "";
+            for (var j = 0; j < $scope.rebusWords.words[i].letters.length; j++) {
+                var value = document.getElementById(i + 'a' + j).value;
+                //console.log(value);
+                input = input + value;
+            }
+            console.log($scope.rebusWords.words[i].value + " ");
+            if (input.toLowerCase() == $scope.rebusWords.words[i].value) {
+                $scope.rebusWords.words[i].correct = true;
+            }
+        }
+    }
+
 	$http({method: 'GET', url: 'php/get_words.php'}).success(function(data) {
 		$scope.words = data;
 		$scope.buildRebus();
